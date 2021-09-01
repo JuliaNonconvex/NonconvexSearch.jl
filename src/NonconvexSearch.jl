cdf9d3f2e4221213f150c8b9a96472e22d4e6713
Mohamed Tarek <mohamed82008@gmail.com>
2021-09-01 16:18:29 +1000

    move from Nonconvex

--- src/NonconvexSearch.jl
+++ src/NonconvexSearch.jl
@@ -1,5 +1,15 @@
 module NonconvexSearch
 
-# Write your package code here.
+export MTS, MTSAlg, LS1Alg, MTSOptions, LS1Options
+
+using Reexport, Parameters
+using Random: randperm
+@reexport using NonconvexCore
+using NonconvexCore: @params, AbstractOptimizer, AbstractResult
+using NonconvexCore: AbstractModel, VecModel, debugging, getdim
+using NonconvexCore: clamp_and_evaluate!
+import NonconvexCore: optimize!, Workspace
+
+include("mts.jl")
 
 end
